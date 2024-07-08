9ca54c36e5bf16ae9ba83dc38687dacc402bdad6
Thiago Valentim <thiago@criawebs.com.br>
2024-07-08 16:08:17 +0300

    fix: Added an adjustment in the test

--- tests/evaluation/test_solution_result.py
+++ tests/evaluation/test_solution_result.py
@@ -9,4 +9,4 @@ def test_solution_result_initialization():
 
 def test_solution_result_repr():
     result = SolutionResult(task="print a hello, world", code="print('Hello, world!')", score=100.0)
-    assert repr(result) == "SolutionResult(task=print a hello, world, code=print('Hello, world!'), score=100.0)"
+    assert repr(result) == "SolutionResult(task=print a hello, world, code=print('Hello, world!'), score=100.0, errors=[])"
