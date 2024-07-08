3be53fb955fbd358c95c2210c9c7c912afd25dbf
Thiago Valentim <thiago@criawebs.com.br>
2024-07-08 18:08:42 +0300

    improvement: applied the codestyle fixes

--- tests/evaluation/test_solution_result.py
+++ tests/evaluation/test_solution_result.py
@@ -1,12 +1,21 @@
 import pytest
 from evaluation.solution_result import SolutionResult
 
+
 def test_solution_result_initialization():
-    result = SolutionResult(task="print a hello, world", code="print('Hello, world!')", score=100.0)
+    result = SolutionResult(
+        task="print a hello, world", code="print('Hello, world!')", score=100.0
+    )
     assert result.task == "print a hello, world"
     assert result.code == "print('Hello, world!')"
     assert result.score == 100.0
 
+
 def test_solution_result_repr():
-    result = SolutionResult(task="print a hello, world", code="print('Hello, world!')", score=100.0)
-    assert repr(result) == "SolutionResult(task=print a hello, world, code=print('Hello, world!'), score=100.0, errors=[])"
+    result = SolutionResult(
+        task="print a hello, world", code="print('Hello, world!')", score=100.0
+    )
+    assert (
+        repr(result)
+        == "SolutionResult(task=print a hello, world, code=print('Hello, world!'), score=100.0, errors=[])"
+    )
